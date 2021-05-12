7d87d25572c56a14edcec01b16aac15a05ef2fa1
yj-2 <dbwls7233@naver.com>
2021-05-13 04:37:01 +0900

    2장

---
+++ Chapter2/src/chapter2/ByteVariableTest.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class ByteVariableTest {
+	public static void main(String[] args) {
+		byte bDate = -128;
+		System.out.println(bDate);
+		
+		byte bData2=127;
+		System.out.println(bData2);
+	}
+}
---
+++ Chapter2/src/chapter2/VariableEx.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+		int num;	//변수를 선언
+		num=10;		//값을 대입
+		
+		System.out.println(num);
+		
+		int level=20;	//선언 및 초기화
+		System.out.println(level);
+		
+		int ns;
+		int numberofStudent;
+	}
+}
