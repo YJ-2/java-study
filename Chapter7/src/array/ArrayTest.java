476f77dfa2afecd1b86596309df9b7a1a48ee6fd
yj-2 <dbwls7233@naver.com>
2021-05-17 21:10:15 +0900

    7장

---
+++ Chapter7/src/array/ArrayTest.java
@@ -0,0 +1,17 @@
+package array;
+
+public class ArrayTest {
+	public static void main(String[] args) {
+		int[] numbers = new int[] {1, 2, 3};
+		
+		for(int i=0; i<3; i++) {
+			System.out.println(numbers[i]);
+		}
+		
+		int[] studentIDs=new int[10];
+		
+		for(int i=0;i<studentIDs.length;i++) {
+			System.out.println(studentIDs[i]);
+		}
+	}
+}
