aced6914ab6c313140c64949933061fa845a3721
yj-2 <dbwls7233@naver.com>
2021-05-26 04:29:42 +0900

    7장

--- Chapter7/src/array/BookArray.java
+++ Chapter7/src/array/BookArray.java
@@ -8,6 +8,16 @@ public class BookArray {
 			System.out.println(library[i]);
 		}
 		
+		library[0]=new Book("태백산맥1", "조정래");
+		library[1]=new Book("태백산맥2", "조정래");
+		library[2]=new Book("태백산맥3", "조정래");
+		library[3]=new Book("태백산맥4", "조정래");
+		library[4]=new Book("태백산맥5", "조정래");
+		
+		for(int i=0; i<library.length; i++) {
+			library[i].showBookInfo();
+		}
+		
 		int[] arr = new int[5];
 	}
 
